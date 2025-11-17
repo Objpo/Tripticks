36677663bda01b5287acfceb4a94933180bd8b07
vo khanh huy <132142974+welshadoew@users.noreply.github.com>
2025-11-17 13:31:22 +0700

    Add files via upload

--- HotelBooking.js
+++ HotelBooking.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const HotelBookingSchema = new mongoose.Schema({
-    // Thông tin người đặt
+    // thong tin
     name: { type: String, required: true },
     email: { type: String, required: true },
     guests: { type: Number, required: true, min: 1 },
@@ -10,29 +10,22 @@ const HotelBookingSchema = new mongoose.Schema({
         required: [true, 'Ngày đặt phòng là bắt buộc.'],
         validate: {
             validator: function (v) {
-                // 1. Lấy ngày hiện tại
                 const now = new Date();
-                // 2. Đặt giờ, phút, giây, mili giây của ngày hiện tại về 0
-                // để chỉ so sánh ngày (date)
                 now.setHours(0, 0, 0, 0);
-
-                // 3. Lấy ngày đặt phòng từ input
                 const bookingDate = new Date(v);
-                // 4. Đặt giờ, phút, giây, mili giây của ngày đặt phòng về 0
                 bookingDate.setHours(0, 0, 0, 0);
-
-                // 5. Kiểm tra: Ngày đặt phòng phải >= Ngày hiện tại
                 return bookingDate >= now;
             },
             message: props => `Ngày đặt phòng (${props.value.toISOString().split('T')[0]}) không thể là ngày trong quá khứ! Vui lòng chọn ngày hiện tại hoặc tương lai.`
         }
     },
+    amount: { type: Number, required: true, default: 0 },
 
-    // Thông tin phòng/khách sạn (lấy từ form)
+
     room_name: { type: String, required: true },
-    hotel_id: { type: String, required: true }, // Lấy từ API /api/hotels
+    hotel_id: { type: String, required: true }, 
 
-    // Trạng thái
+
     booking_status: {
         type: String,
         required: true,
@@ -40,7 +33,7 @@ const HotelBookingSchema = new mongoose.Schema({
     },
 }, {
     timestamps: true,
-    collection: 'hotel_bookings' // 💡 Chỉ định rõ tên collection
+    collection: 'hotel_bookings' 
 });
 
 const HotelBooking = mongoose.models.HotelBooking || mongoose.model("HotelBooking", HotelBookingSchema);
