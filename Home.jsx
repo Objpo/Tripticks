dccf5b3c4e5162558ae81d20ab1b139dc0d1dfb5
vo khanh huy <132142974+welshadoew@users.noreply.github.com>
2025-11-17 14:09:13 +0700

    Add files via upload

--- Home.jsx
+++ Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/navbar";
+  import Navbar from "../components/navbar";
 import Hero from "../components/Hero";
 import SearchSection from "../components/SearchSection";
 import Services from "../components/Services";
