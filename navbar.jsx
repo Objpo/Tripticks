5d2281c89979394a4a716bc749cd01035e688207
vo khanh huy <132142974+welshadoew@users.noreply.github.com>
2025-11-17 14:02:33 +0700

    Add files via upload

--- navbar.jsx
+++ navbar.jsx
@@ -118,10 +118,6 @@ const Navbar = () => {
                   <Link className="dropdown-item" to="/profile">
                     Profile
                   </Link>
-                  {/* 💡 ĐÃ THÊM: Mục Payment */}
-                  <Link className="dropdown-item" to="/payment">
-                    Payment
-                  </Link>
                   <div className="dropdown-divider"></div>
                   <button className="dropdown-item" onClick={handleLogout}>
                     Logout
