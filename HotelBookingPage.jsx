2ca2076c0749ccbd789c338541bbc74dfba9c148
vo khanh huy <132142974+welshadoew@users.noreply.github.com>
2025-11-17 14:09:42 +0700

    Add files via upload

--- HotelBookingPage.jsx
+++ HotelBookingPage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/Footer";
-// 💡 1. Import component booking khách sạn mới của bạn
+
 import HotelBookingContent from "../components/HotelBookingContent";
 
 const HotelBookingPage = () => {
     return (
         <div>
             <Navbar />
-            {/* 💡 2. Sử dụng component booking khách sạn ở đây */}
+           
             <HotelBookingContent />
             <Footer />
         </div>
